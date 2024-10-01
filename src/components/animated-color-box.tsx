8038efdd8f06feb0cf3b5bb1f74815f1ed89fd22
Dung Lu <dung@mylong.io>
2024-10-01 23:16:13 +0700

    Feat: task list

---
+++ src/components/animated-color-box.tsx
@@ -0,0 +1,35 @@
+import { Box, useToken } from 'native-base';
+import React, { useEffect } from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolateColor,
+} from 'react-native-reanimated';
+import usePrevious from '../utils/use-previous';
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+const AnimatedColorBox = ({ bg, ...props }: any) => {
+  const hexBg = useToken('colors', bg);
+  const prevHexBg = usePrevious(hexBg);
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = 0;
+  }, [hexBg]);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    progress.value = withTiming(1, { duration: 200 });
+    return {
+      backgroundColor: interpolateColor(
+        progress.value,
+        [0, 1],
+        [prevHexBg || hexBg, hexBg],
+      ),
+    };
+  }, [hexBg]);
+  return <AnimatedBox {...props} style={animatedStyles} />;
+};
+
+export default AnimatedColorBox;
